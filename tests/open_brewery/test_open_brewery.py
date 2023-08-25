ed237fa440e549e6a90cc99ccaf1637595c50ccf
oksanacps <Ksundeli76@gmail.com>
2023-08-25 22:04:17 +0300

    1) added test for open brewery

--- tests/open_brewery/test_open_brewery.py
+++ tests/open_brewery/test_open_brewery.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.base_request import BaseRequest
+from test_data import url_data
 
 
 @pytest.mark.brewery
@@ -8,14 +9,30 @@ class TestOpenBrewery:
     """
 
     """
+    @pytest.mark.parametrize('brewery_type', [
+        'micro',
+        'nano',
+        'regional',
+        'brewpub',
+        'large',
+        'planning',
+        'contract',
+        'proprietor',
+        'closed'
+    ])
+    def test_brewery_type(self, brewery_type):
+        base_request = BaseRequest(url_data.BASE_URL_OPEN_BREWERY)
+        response = base_request.get(f'?by_type={brewery_type}&per_page=3')
+        for brewery in response:
+            assert brewery['brewery_type'] == brewery_type
 
-    def test_open_brewery1(self):
-        base_request = BaseRequest(test_data.BASE_URL_DOG_CEO)
-        response = base_request.get('example', 1)
-        assert response.ok
-
-    def test_open_brewery2(self):
-        pass
+    @pytest.mark.parametrize('search', ['dog', 'GREEN'])
+    def test_search_not_case_sensitive(self, search):
+        base_request = BaseRequest(url_data.BASE_URL_OPEN_BREWERY)
+        response = base_request.get(f'search?query={search}&per_page=3')
+        for brewery in response:
+            assert search.lower() in brewery['name'].lower()
+        # скорее всего если регистрозависимость, то нужно с одним именем найти и сравнить что нашлось одинковое
 
     def test_open_brewery3(self):
         pass
